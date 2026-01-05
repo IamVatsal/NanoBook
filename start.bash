192a0a9930b78f480d7b90806fd7a76eaaafc91f
Vatsal Patel <vatsalpatel0609@gmail.com>
2026-01-05 19:08:10 +0530

    feat: update start.bash to use docker start and modify backend server command

--- start.bash
+++ start.bash
@@ -1,14 +1,14 @@
 # !/bin/bash
 
 # Start Qdrant vector database in a Docker container
-docker run -p 6333:6333 --storage-path /qdrant/storage --name notebook_db qdrant/qdrant:latest &
+docker start -p 6333:6333 --name notebook_db qdrant/qdrant:latest &
 
 # start backend server
 cd nanobook-backend
-python3 -m venv venv
+# python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port 5000 &
+python src/app.py &
 deactivate
 cd ..
 
